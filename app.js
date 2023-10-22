95dc27cbf9caab6d05c3b208dca34f07302191eb
André Luiz Lourenço Junior <109436843+AndreJuniorAJ@users.noreply.github.com>
2023-10-22 13:38:35 -0300

    Atualizando
    Subindo o código

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// Altera o texto do título
-document.querySelector("h1").textContent = "Programação e Sistemas de Informação";
-// Adiciona um link para o GitHub
-document.querySelector("footer").innerHTML += `
-<a
-href="https://github.com/bard/explorando-o-mundo-tecnologico">GitHub</a
->
-`;
+let name = prompt("Informe seu nome:");
+
+alert(`Seja bem-vindo ${name}`)
